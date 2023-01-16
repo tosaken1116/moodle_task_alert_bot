96822243bd1ce35e6c25c9e0ef4d00c6677f3bc8
tosaken1116 <nagato20021122@gmail.com>
2023-01-17 00:27:44 +0900

    fix:日付フォーマットによるタスクが正しく取得されないバグの修正

--- methods.py
+++ methods.py
@@ -19,13 +19,13 @@ def get_date_format(detect_date):
     w_list = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日']
     if detect_date =="today":
         return [
-            f"{now.year}年 {now.month}月 {now.day}日({w_list[now.weekday()]})"
+            f"{now.year}年 {f'0{now.month}'[-2:]}月 {now.day}日({w_list[now.weekday()]})"
             ]
     elif detect_date =="tomorrow":
         return  [
-            f"{now.year}年 {now.month}月 {now.day+1}日({w_list[now.weekday()+1]})"
+            f"{now.year}年 {f'0{now.month}'[-2:]}月 {now.day+1}日({w_list[now.weekday()+1]})"
             ]
     elif  re.match(".*?(\d+)", detect_date):
         print([f"{(now+datetime.timedelta(days=i)).year}年 {(now+datetime.timedelta(days=i)).month}月 {(now+datetime.timedelta(days=i)).day}日({w_list[(now+datetime.timedelta(days=i)).weekday()]})" for i in range(int(re.sub("[^\d]","", detect_date)))])
-        return [f"{(now+datetime.timedelta(days=i)).year}年 {(now+datetime.timedelta(days=i)).month}月 {(now+datetime.timedelta(days=i)).day}日({w_list[(now+datetime.timedelta(days=i)).weekday()]})" for i in range(int(re.sub("[^\d]","", detect_date)))]
+        return [f"{(now+datetime.timedelta(days=i)).year}年 {f'0{(now+datetime.timedelta(days=i)).month}'[-2:]}月 {(now+datetime.timedelta(days=i)).day}日({w_list[(now+datetime.timedelta(days=i)).weekday()]})" for i in range(int(re.sub("[^\d]","", detect_date)))]
 
